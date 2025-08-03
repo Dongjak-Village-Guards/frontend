0d92445fe2af3bda25ad8354b91734fc52ac9fb7
xaerinoo <xaerinoo@naver.com>
2025-08-04 03:37:29 +0900

    Feat: 로그인, 주소 검색

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import SearchAddress from './pages/SearchAddress';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/search-address' element={<SearchAddress />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// ' /search-address ' 바꾸면 안됨요 !!!!!!! 여기다가 API 연결함
 
 export default App;
