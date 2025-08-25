d30e1b9a4d75cd20ae16ee6872972d6b50257d9c
xaerinoo <xaerinoo@naver.com>
2025-08-25 14:17:10 +0900

    !HOTFIX: netlify deploy key 노출 문제 해결 시도

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@
 
 import './App.css';
 import { useEffect } from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainPageApp from './pages/MainPageApp';
 import useUserInfo from './hooks/user/useUserInfo';
@@ -42,7 +41,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
         <Router>
           <Routes>
             {/* 모든 페이지를 MainPageApp으로 통합 */}
@@ -68,7 +66,6 @@ function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
-      </GoogleOAuthProvider>
     </>
   );
 }
